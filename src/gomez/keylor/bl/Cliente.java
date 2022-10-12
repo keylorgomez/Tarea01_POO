b303c86e75e67f0044e38d0e11d48422d660a5c7
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-12 11:48:41 -0600

    Creando el metodo toString en la clase Cliente

--- src/gomez/keylor/bl/Cliente.java
+++ src/gomez/keylor/bl/Cliente.java
@@ -1,4 +1,76 @@
 package gomez.keylor.bl;
 
 public class Cliente {
+    // Creando Atributos clase Cliente
+    private String nombre;
+    private String identificacion;
+    private String fechaNacimiento;
+    private int edad;
+    private String direccion;
+
+    //Creando los getters and setters
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
+    //Creando un constructor que reciba todos los parametros
+
+    public Cliente(String nombre, String identificacion, String fechaNacimiento, int edad, String direccion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+    
+    //Creando el toString
+
+    @Override
+    public String toString() {
+        return "\nCliente: \n" +
+                "Nombre: " + nombre + "\n" +
+                "Número de identificacion: " + identificacion + "\n" +
+                "Fecha de Nacimiento: " + fechaNacimiento + "\n" +
+                "Edad: " + edad +"\n"+
+                "Direccion: " + direccion+"\n \n";
+    }
 }
