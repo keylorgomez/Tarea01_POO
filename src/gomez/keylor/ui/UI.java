9013c9ab96e68876dda10e38555b8f911759bcfb
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-12 14:01:11 -0600

    Se crean las instancias para realizar depositos, retiros y consultas de saldo, enlazados a los metos establecidos en la clase Banco

--- src/gomez/keylor/ui/UI.java
+++ src/gomez/keylor/ui/UI.java
@@ -2,6 +2,7 @@ package gomez.keylor.ui;
 
 import gomez.keylor.bl.Banco;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class UI {
@@ -64,4 +65,69 @@ public class UI {
             System.out.println(clienteTemp);
         }
     }
+
+    public static void nuevaCuenta(){
+        Random numeroAleatorio= new Random();
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el número de identificación del cliente: ");
+        String identificacion = entrada.nextLine();
+
+        // Calculo del número de cuenta
+        int n=numeroAleatorio.nextInt(1234567-1026849)+1026849;
+        String numeroCuenta= String.valueOf(n);
+
+        System.out.print("Ingrese el saldo que va a tener la cuenta (Debe ser mayor o igual a ₡50.000 colones): ");
+        double saldo=entrada.nextDouble();
+
+        String resultado=Banco.CrearCuenta(identificacion,numeroCuenta,saldo);
+        System.out.println(resultado);
+    }
+
+    public  static void RealizarDeposito(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el número de cuenta del cliente: ");
+        String numeroCuenta=entrada.nextLine();
+        System.out.print("Ingrese la cantidad que desea depositar: ₡");
+        int deposito=entrada.nextInt();
+
+        double resultado=Banco.GenerarDeposito(numeroCuenta,deposito);
+        if (resultado>0){
+            System.out.println("El depósito se realizó con éxito");
+        }else{
+            System.out.println("NO se pudo realizar el depósito. Algún dato se ingresó de manera erronea");
+        }
+    }
+
+    public static void RealizarRetiro(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el número de cuenta del cliente: ");
+        String numeroCuenta=entrada.nextLine();
+        System.out.print("Ingrese la cantidad que desea retirar: ₡");
+        int retiro=entrada.nextInt();
+
+        double resultado=Banco.GenerarRetiro(numeroCuenta,retiro);
+        if (resultado==-1){
+            System.out.println("No se pueden realizar retiros superiores a la cantidad de dinero que posee la cuenta.");
+        }else if(resultado==0){
+            System.out.println("El numero de cuenta ingresado no se encuentra registrado en el sistema.");
+        }else{
+            System.out.println("El retiro se realizó con éxito");
+        }
+    }
+
+    public static void ConsultarSaldo(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el número de cuenta del cliente: ");
+        String numeroCuenta=entrada.nextLine();
+
+        double resultado=Banco.MostrarSaldo(numeroCuenta);
+        if (resultado>0){
+            System.out.println("Su saldo en la cuenta es: ₡"+resultado);
+        }else if(resultado==0){
+            System.out.println("Su cuenta no posee fondos");
+        } else {
+            System.out.println("El numero de cuenta ingresado no se encuentra registrado en el sistema.");
+        }
+    }
+
 }
