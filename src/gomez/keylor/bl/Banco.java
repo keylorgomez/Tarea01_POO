8230d5b995df565ba42b435f3fdcba4d60d9e3b2
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-12 12:07:24 -0600

    Creando los arraylist correspondientes para almacernar información necesaria

---
+++ src/gomez/keylor/bl/Banco.java
@@ -0,0 +1,14 @@
+package gomez.keylor.bl;
+
+import java.util.ArrayList;
+
+public class Banco {
+    //Creando ArrayList para almacenar info importante
+    static ArrayList<Cliente> listaClientes=new ArrayList<>();
+    static ArrayList<Cuenta> listaCuentas=new ArrayList<>();
+    static ArrayList<String> listaCedulas=new ArrayList<>();
+    static ArrayList <String> listanumeroCuenta= new ArrayList<>();
+    static ArrayList <Double> listaSaldosCuentas= new ArrayList<>();
+
+    
+}
