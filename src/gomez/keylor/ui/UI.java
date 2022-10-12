7c7e90a222de969b3d21c15419cf064c95517f0d
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-12 11:43:11 -0600

    Creando getters and setters, además del constructor en la clase Cliente

--- src/gomez/keylor/ui/UI.java
+++ src/gomez/keylor/ui/UI.java
@@ -7,4 +7,61 @@ public class UI {
     private String fechaNacimiento;
     private int edad;
     private String direccion;
+
+    //Generando getters and setters
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
+    //Generando Constructor
+
+
+    public UI(String nombre, String identificacion, String fechaNacimiento, int edad, String direccion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    
 }
