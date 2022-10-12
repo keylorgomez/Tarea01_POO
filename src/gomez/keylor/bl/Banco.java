2ce0b2fb389e9a5c1fb64005e78c192672ae9638
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-12 12:10:46 -0600

    Creacion del metodo RegistrarClientes() el cual permite crear un nuevo cliente

--- src/gomez/keylor/bl/Banco.java
+++ src/gomez/keylor/bl/Banco.java
@@ -10,5 +10,25 @@ public class Banco {
     static ArrayList <String> listanumeroCuenta= new ArrayList<>();
     static ArrayList <Double> listaSaldosCuentas= new ArrayList<>();
 
-    
+    public static String RegistrarCliente(String nombre, String identificacion, String fechaNacimiento, int edad, String direccion){
+        boolean validarCed=false;
+        for (int i=0;i<listaCedulas.size();i++){
+            if (identificacion.equals(listaCedulas.get(i))){
+                validarCed=true;
+                break;
+            }
+        }
+        if (validarCed==false){
+            Cliente cliente1 = new Cliente(nombre,identificacion,fechaNacimiento,edad,direccion);
+            listaClientes.add(cliente1);
+            listaCedulas.add(cliente1.getIdentificacion());
+
+
+            return "\nEl cliente: "+cliente1.getNombre()+", número de cédula: "+cliente1.getIdentificacion()+" ha sido registrado exitosamente.";
+        }else{
+            return "\nUn cliente con la cédula: "+ identificacion+" ya se encuentra regitrado, por lo que NO se puede registrar otra vez";
+        }
+    }
+
+
 }
