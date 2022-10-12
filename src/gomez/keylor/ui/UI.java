80fb54c95bfaa5f3df109be857209408f22a2722
Keylor Gómez Rodríguez <key.go.1101@gmail.com>
2022-10-12 12:55:32 -0600

    Instanciando el main para ejecutar la funcion menu, además creando la función menu que permite ver todas las opciones que existen en el sistema

--- src/gomez/keylor/ui/UI.java
+++ src/gomez/keylor/ui/UI.java
@@ -1,4 +1,28 @@
 package gomez.keylor.ui;
 
+import java.util.Scanner;
+
 public class UI {
+
+    public static void main(String[] args) {
+        SeleccionarOpcion();
+    }
+
+    public static void SeleccionarOpcion(){
+        Scanner entrada = new Scanner(System.in);
+        int opcion = -1;
+        do {
+            System.out.println("\n**** Sistema de registro de Propiedades *****");
+            System.out.println("1. Registrar cliente.");
+            System.out.println("2. Listar clientes.");
+            System.out.println("3. Crear cuenta.");
+            System.out.println("4. Realizar depósito.");
+            System.out.println("5. Realizar retiro.");
+            System.out.println("6. Mostrar saldo.");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = entrada.nextInt();
+            //EjecutarOpcion(opcion);
+        } while (opcion != 0);
+    }
 }
